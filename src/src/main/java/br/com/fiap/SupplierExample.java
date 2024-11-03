ef36b4d806790c51413151049dc967751da26cb7
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-02 21:55:19 -0300

    Adicionando Supplier

---
+++ src/src/main/java/br/com/fiap/SupplierExample.java
@@ -0,0 +1,19 @@
+package br.com.fiap;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<Integer> gerarNumeroAleatorio = () -> new Random().nextInt(100);
+
+        List<Integer> numerosAleatorios = Stream.generate(gerarNumeroAleatorio)
+                .limit(10)
+                .collect(Collectors.toList());
+
+        numerosAleatorios.forEach(System.out::println);
+    }
+}
