b03b68ff26873c2f713b4b336544dd26e1acb0d2
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-03 20:44:30 -0300

    Adicionando Lista Tarefa

---
+++ src/main/java/br/com/fiap/list/basico/Tarefa.java
@@ -0,0 +1,18 @@
+package br.com.fiap.list.basico;
+
+public class Tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao ;
+    }
+}
