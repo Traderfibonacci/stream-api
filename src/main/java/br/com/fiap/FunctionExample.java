053fd67974714a08627d7f2a661b0dc19695c5f1
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-02 22:27:01 -0300

    Adicionando Stream api

---
+++ src/main/java/br/com/fiap/FunctionExample.java
@@ -0,0 +1,19 @@
+package br.com.fiap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 25, 100);
+
+        Function<Integer, Integer>  dobrar = numero -> numero * 2;
+
+        List<Integer> numerosDobrados = numeros
+                .stream()
+                .map(dobrar)
+                .toList();
+        numerosDobrados.forEach(System.out::println);
+    }
+}
---
+++ src/main/java/br/com/fiap/PredicateExample.java
@@ -0,0 +1,17 @@
+package br.com.fiap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args) {
+        List<String> palavras = Arrays.asList("java", "kotlin", "javascript", "python");
+
+        Predicate<String> menosDeCincoCaracteres = palavra -> palavra.length() < 5;
+
+        palavras.stream()
+                .filter(menosDeCincoCaracteres)
+                .forEach(System.out::println);
+    }
+}
