35f5e304572029d4dfc85673f1489df5bd2d11ca
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-08 23:21:13 -0300

    Adicionando map, ordenacao

---
+++ src/main/java/br/com/fiap/map/ordenacao/AgendaEventos.java
@@ -0,0 +1,56 @@
+package br.com.fiap.map.ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class AgendaEventos {
+
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+        Evento evento = new Evento(nome, atracao);
+        eventosMap.put(data, evento);
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        Optional<Map.Entry<LocalDate, Evento>> proximoEvento = eventosMap.entrySet().stream()
+                .filter(entry -> !entry.getKey().isBefore(dataAtual))
+                .findFirst();
+
+        if (proximoEvento.isPresent()) {
+            System.out.println("O próximo evento: " + proximoEvento.get().getValue().getNome() +
+                    " acontecerá na data " + proximoEvento.get().getKey());
+        } else {
+            System.out.println("Não há eventos futuros na agenda.");
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Conferência de Tecnologia", "Palestrante renomado");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.AUGUST, 28), "Hackathon de Inovação", "Competição de soluções criativas");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 11, 8), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
+
+
+        agendaEventos.exibirAgenda();
+
+
+        agendaEventos.obterProximoEvento();
+    }
+}
+
--- src/main/java/br/com/fiap/map/pesquisa/EstoqueProdutos.java
+++ src/main/java/br/com/fiap/map/pesquisa/EstoqueProdutos.java
@@ -1,5 +1,6 @@
 package br.com.fiap.map.pesquisa;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,52 +20,29 @@ public class EstoqueProdutos {
     }
 
     public double calcularValorTotalEstoque() {
-        double valorTotalEstoque = 0d;
-        if (!estoqueProdutosMap.isEmpty()) {
-            for (Produto p : estoqueProdutosMap.values()) {
-                valorTotalEstoque += p.getQuantidade() * p.getPreco();
-            }
-        }
-        return valorTotalEstoque;
+        return estoqueProdutosMap.values()
+                .stream()
+                .mapToDouble(p -> p.getQuantidade() * p.getPreco())
+                .sum();
     }
 
     public Produto obterProdutoMaisCaro() {
-        Produto produtoMaisCaro = null;
-        double maiorPreco = Double.MIN_VALUE;
-        for (Produto p : estoqueProdutosMap.values()) {
-            if (p.getPreco() > maiorPreco) {
-                produtoMaisCaro = p;
-                maiorPreco = p.getPreco();
-            }
-        }
-        return produtoMaisCaro;
+        return estoqueProdutosMap.values()
+                .stream()
+                .max(Comparator.comparingDouble(Produto::getPreco))
+                .orElse(null);
     }
 
     public Produto obterProdutoMaisBarato() {
-        Produto produtoMaisBarato = null;
-        double menorPreco = Double.MAX_VALUE;
-        for (Produto p : estoqueProdutosMap.values()) {
-            if (p.getPreco() < menorPreco) {
-                produtoMaisBarato = p;
-                menorPreco = p.getPreco();
-            }
-        }
-        return produtoMaisBarato;
+        return estoqueProdutosMap.values().stream()
+                .min(Comparator.comparingDouble(Produto::getPreco))
+                .orElse(null);
     }
 
     public Produto obterProdutoMaiorQuantidadeValorTotalNoEstoque() {
-        Produto produtoMaiorQuantidadeValorNoEstoque = null;
-        double maiorValorTotalProdutoEstoque = 0d;
-        if (!estoqueProdutosMap.isEmpty()) {
-            for (Map.Entry<Long, Produto> entry : estoqueProdutosMap.entrySet()) {
-                double valorProdutoEmEstoque = entry.getValue().getPreco() * entry.getValue().getQuantidade();
-                if (valorProdutoEmEstoque > maiorValorTotalProdutoEstoque) {
-                    maiorValorTotalProdutoEstoque = valorProdutoEmEstoque;
-                    produtoMaiorQuantidadeValorNoEstoque = entry.getValue();
-                }
-            }
-        }
-        return produtoMaiorQuantidadeValorNoEstoque;
+       return estoqueProdutosMap.values().stream()
+               .max(Comparator.comparingDouble(p -> p.getPreco() * p.getQuantidade()))
+               .orElse(null);
     }
 
     public static void main(String[] args) {
@@ -74,10 +52,10 @@ public class EstoqueProdutos {
         estoque.exibirProdutos();
 
 
-        estoque.adicionarProduto(1L, "Notebook", 1, 1500.0);
+        estoque.adicionarProduto(1L, "Notebook", 5, 1500.0);
         estoque.adicionarProduto(2L, "Mouse", 5, 25.0);
-        estoque.adicionarProduto(3L, "Monitor", 10, 400.0);
-        estoque.adicionarProduto(4L, "Teclado", 2, 40.0);
+        estoque.adicionarProduto(3L, "Monitor", 2, 400.0);
+        estoque.adicionarProduto(4L, "Teclado", 3, 40.0);
 
 
         estoque.exibirProdutos();
